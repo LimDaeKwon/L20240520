21f087df35d5a0ff77030c05085450ec433220c1
LDK <gritdrake@gmail.com>
2024-05-20 10:19:40 +0900

    Init

---
+++ main.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int GetCard(string& CardShape);
+void Render();
+void PrintResult(int Computer, int Player);
+bool CheckOne(int Sum , int Card1 , int Card2);
+
+bool CheckOne(int Sum, int Card1, int Card2)
+{
+	return Sum < 12 && (Card1 == 1 || Card2 == 1);
+}
+
+void PrintResult(int Computer, int Player)
+{
+	if (Computer > Player) cout << "컴퓨터 승리!!" << endl;
+	else if (Computer < Player) cout << "플레이어 승리!!" << endl;
+	else cout << "무승부!!" << endl;
+}
+
+void Render()
+{
+	string ComputerCard1Shape;
+	string ComputerCard2Shape;
+	int ComputerCard1 = GetCard(ComputerCard1Shape);
+	int ComputerCard2 = GetCard(ComputerCard2Shape);
+
+	string PlayerCard1Shape;
+	string PlayerCard2Shape;
+	int PlayerCard1 = GetCard(PlayerCard1Shape);
+	int PlayerCard2 = GetCard(PlayerCard2Shape);
+	
+
+
+	cout << "블랙잭 게임을 시작합니다." << endl;
+	
+	int SumComputerCard = ComputerCard1 + ComputerCard2;
+	int SumPlayerCard = PlayerCard1 + PlayerCard2;
+
+	if (CheckOne(SumComputerCard , ComputerCard1, ComputerCard2)) SumComputerCard += 10;
+	if (CheckOne(SumPlayerCard, PlayerCard1, PlayerCard2)) SumPlayerCard += 10;
+
+	cout << "컴퓨터 카드 : " << ComputerCard1Shape << " , " << ComputerCard2Shape << " 합 : " << SumComputerCard << endl;
+	cout << "플레이어 카드 : " << PlayerCard1Shape << " , " << PlayerCard2Shape << " 합 : " << SumPlayerCard << endl;
+
+
+	PrintResult(SumComputerCard, SumPlayerCard);
+	
+}
+
+int GetCard(string& CardShape)
+{
+	int Card = rand() % 13 + 1;
+
+	switch (Card)
+	{
+	case 10:
+		CardShape = "10";
+		break;
+	case 11:
+		CardShape = "J";
+		break;
+	case 12:
+		CardShape = "Q";
+		break;
+	case 13:
+		CardShape = "K";
+		break;
+	default :
+		CardShape = Card + 48;
+	}
+	
+	if (Card > 10)
+	{
+		Card = 10;
+	}
+	return Card ;
+
+}
+
+int main()
+{
+	srand(time(NULL));
+	Render();
+	
+	return 0;
+}
